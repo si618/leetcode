f07a022c4c4feff84bb47ec79c00438d6e145e37
Simon McKenna <sshnug.si+github@gmail.com>
2022-04-09 08:56:37 +0930

    Refactor to more easily understand

--- Submissions/LengthOfLongestSubstring.cs
+++ Submissions/LengthOfLongestSubstring.cs
@@ -7,25 +7,21 @@ public sealed partial class Submission
 {
     public static int LengthOfLongestSubstring(string s)
     {
-        var map = new Dictionary<char, int>();
+        var list = new List<char>();
         var left = 0;
         var max = 0;
 
-        // Uses "sliding window" solution
         for (var right = 0; right < s.Length; right++)
         {
-            var current = s[right];
-
-            if (!map.TryAdd(current, right))
+            // Uses "sliding window" solution
+            // https://youtu.be/wiGpQwVHdE0
+            while (list.Contains(s[right]))
             {
-                // Slide window past character
-                // https://youtu.be/wiGpQwVHdE0?t=208
-                left = Math.Max(map[current] + 1, left);
-                // Update characters position in window
-                map[current] = right;
+                list.Remove(s[left]);
+                left++;
             }
 
-            // Update if a longer length is found
+            list.Add(s[right]);
             max = Math.Max(max, right - left + 1);
         }
 
