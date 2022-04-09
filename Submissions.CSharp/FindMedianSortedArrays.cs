381aa3f0ad4cc49ba71c994e728c37af1303163f
Simon McKenna <sshnug.si+github@gmail.com>
2022-04-09 15:54:17 +0930

    Leave submission for later

--- Submissions.CSharp/FindMedianSortedArrays.cs
+++ Submissions.CSharp/FindMedianSortedArrays.cs
@@ -1,24 +1,25 @@
 ﻿namespace LeetCode;
 
-using FluentAssertions;
-using NUnit.Framework;
+//using FluentAssertions;
+//using NUnit.Framework;
 
 public sealed partial class Submission
 {
-    [LeetCode(Difficulty.Hard, Category.BinarySearch)]
-    public static double FindMedianSortedArrays(int[] nums1, int[] nums2)
-    {
-        return 0;
-    }
+    // TODO: Waiting until I've done other easy and medium binary search submissions
+    //[LeetCode(Difficulty.Hard, Category.BinarySearch)]
+    //public static double FindMedianSortedArrays(int[] nums1, int[] nums2)
+    //{
+    //    return 0;
+    //}
 
-    [Test]
-    public void FindMedianSortedArraysTest()
-    {
-        var ex1nums1 = new[] { 1, 3 };
-        var ex1nums2 = new[] { 2 };
-        var ex2nums1 = new[] { 1, 2 };
-        var ex2nums2 = new[] { 3, 4 };
-        FindMedianSortedArrays(ex1nums1, ex1nums2).Should().Be(2d);
-        FindMedianSortedArrays(ex2nums1, ex2nums2).Should().Be(2.5d);
-    }
+    //[Test]
+    //public void FindMedianSortedArraysTest()
+    //{
+    //    var ex1nums1 = new[] { 1, 3 };
+    //    var ex1nums2 = new[] { 2 };
+    //    var ex2nums1 = new[] { 1, 2 };
+    //    var ex2nums2 = new[] { 3, 4 };
+    //    FindMedianSortedArrays(ex1nums1, ex1nums2).Should().Be(2d);
+    //    FindMedianSortedArrays(ex2nums1, ex2nums2).Should().Be(2.5d);
+    //}
 }
