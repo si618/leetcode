08c9c4620835c44fc6ddb8f3e16e4bf4b837d812
Simon McKenna <sshnug.si+github@gmail.com>
2022-04-10 11:22:51 +0930

    Add F# benchmark to workflow

--- Benchmarks.FSharp/Challenges/FizzBuzz.fs
+++ Benchmarks.FSharp/Challenges/FizzBuzz.fs
@@ -6,6 +6,8 @@ open BenchmarkDotNet.Engines
 [<MemoryDiagnoser>]
 type Benchmark() =
     let ex1 = LeetCode.Submission.FizzBuzz 3
+    let ex2 = LeetCode.Submission.FizzBuzz 5
+    let ex3 = LeetCode.Submission.FizzBuzz 15
 
     [<Benchmark>]
     member _.FizzBuzz() = ex1.Consume
