e9739ddbeeb9dd65c3afc9a64abcf8bdf4990c69
Simon McKenna <sshnug.si+github@gmail.com>
2022-04-24 15:52:58 +0930

    Sort alphabetically

--- Benchmarks.FSharp/Benchmarks.fs
+++ Benchmarks.FSharp/Benchmarks.fs
@@ -7,13 +7,13 @@ open BenchmarkDotNet.Attributes
 type FSharpBenchmarks() =
 
     [<Benchmark>]
-    member _.FizzBuzz() = [ 
-        LeetCode.Submission.FizzBuzz 3;
-        LeetCode.Submission.FizzBuzz 5; 
-        LeetCode.Submission.FizzBuzz 15 ]
-
-    [<Benchmark>]
     member _.ContainsDuplicate() = [
         LeetCode.Submission.ContainsDuplicate [| 1; 2; 3; 1 |]; 
         LeetCode.Submission.ContainsDuplicate [| 1; 2; 3; 4 |]; 
         LeetCode.Submission.ContainsDuplicate [| 1; 1; 1; 3; 3; 4; 3; 2; 4; 2 |] ]
+
+    [<Benchmark>]
+    member _.FizzBuzz() = [ 
+        LeetCode.Submission.FizzBuzz 3;
+        LeetCode.Submission.FizzBuzz 5; 
+        LeetCode.Submission.FizzBuzz 15 ]
