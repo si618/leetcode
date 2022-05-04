df3b9c22f0180b2fcdef427685646deff1cf7d49
Simon McKenna <sshnug.si+github@gmail.com>
2022-05-05 09:07:02 +0930

    Split lines for readabilty and no build needed

--- Benchmark.ps1
+++ Benchmark.ps1
@@ -1,3 +1,6 @@
-dotnet build --configuration Release
-dotnet run --project Benchmarks.CSharp/Benchmarks.CSharp.csproj --configuration Release --filter LeetCode.CSharpBenchmarks* --memory --exporters json --warmupCount 3 --minIterationCount 6 --maxIterationCount 9
-dotnet run --project Benchmarks.FSharp/Benchmarks.FSharp.fsproj --configuration Release --filter LeetCode.FSharpBenchmarks* --memory --exporters json --warmupCount 3 --minIterationCount 6 --maxIterationCount 9
+dotnet run --project Benchmarks.CSharp/Benchmarks.CSharp.csproj --configuration Release `
+    --filter LeetCode.CSharpBenchmarks* --memory --exporters json `
+    --warmupCount 3 --minIterationCount 6 --maxIterationCount 9
+dotnet run --project Benchmarks.FSharp/Benchmarks.FSharp.fsproj --configuration Release `
+    --filter LeetCode.FSharpBenchmarks* --memory --exporters json `
+    --warmupCount 3 --minIterationCount 6 --maxIterationCount 9
