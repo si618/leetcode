949050acca7a9561b09dcbe3e2e721458c304571
Simon McKenna <sshnug.si+github@gmail.com>
2022-05-15 16:02:41 +0930

    Add "Valid Anagram" challenge

---
+++ Benchmarks.CSharp/Benchmarks/IsAnagram.cs
@@ -0,0 +1,15 @@
+﻿// ReSharper disable once CheckNamespace
+namespace LeetCode;
+
+using BenchmarkDotNet.Attributes;
+
+public partial class CSharpBenchmarks
+{
+    [Benchmark]
+    public void IsAnagram()
+    {
+        // ReSharper disable once StringLiteralTypo
+        Submission.IsAnagram("anagram", "nagaram");
+        Submission.IsAnagram("rat", "car");
+    }
+}
