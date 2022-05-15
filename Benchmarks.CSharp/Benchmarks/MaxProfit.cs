4fd2947e99b2de50735ab9bcf3297afa016a5127
Simon McKenna <sshnug.si+github@gmail.com>
2022-05-15 16:40:33 +0930

    Add "Best Time to Buy and Sell Stock" challenge

---
+++ Benchmarks.CSharp/Benchmarks/MaxProfit.cs
@@ -0,0 +1,14 @@
+﻿// ReSharper disable once CheckNamespace
+namespace LeetCode;
+
+using BenchmarkDotNet.Attributes;
+
+public partial class CSharpBenchmarks
+{
+    [Benchmark]
+    public void MaxProfit()
+    {
+        Submission.MaxProfit(new[] { 7, 1, 5, 3, 6, 4 });
+        Submission.MaxProfit(new[] { 7, 6, 4, 3, 1 });
+    }
+}
