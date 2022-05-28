e971d467f3b7ef5018903001dbce54dfe5a82815
Simon McKenna <sshnug.si+github@gmail.com>
2022-05-28 14:18:27 +0930

    Add "Valid Parentheses" challenge

---
+++ Benchmarks.CSharp/Benchmarks/ValidParentheses.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode;
+
+using BenchmarkDotNet.Attributes;
+
+public partial class CSharpBenchmarks
+{
+    [Benchmark]
+    public void AreParenthesesValid()
+    {
+        Submission.AreParenthesesValid("()");
+        Submission.AreParenthesesValid("()[]{}");
+        Submission.AreParenthesesValid("(]");
+    }
+}
