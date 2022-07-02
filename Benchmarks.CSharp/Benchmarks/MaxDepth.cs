ee32da664980fbbfe3d413a759741b45c27b1fe5
Simon McKenna <sshnug.si+github@gmail.com>
2022-07-02 17:43:41 +0930

    Add "Maximum Depth of Binary Tree" challenge

---
+++ Benchmarks.CSharp/Benchmarks/MaxDepth.cs
@@ -0,0 +1,19 @@
+﻿// ReSharper disable once CheckNamespace
+namespace LeetCode;
+
+using BenchmarkDotNet.Attributes;
+
+public partial class CSharpBenchmarks
+{
+    [Benchmark]
+    public void MaxDepth()
+    {
+        var root1 = new TreeNode(3,
+            left: new TreeNode(9, left: new TreeNode(1), right: null),
+            right: new TreeNode(20, left: new TreeNode(15), right: new TreeNode(7)));
+        var root2 = new TreeNode(1, left: null, right: new TreeNode(2));
+
+        Submission.MaxDepth(root1);
+        Submission.MaxDepth(root2);
+    }
+}
