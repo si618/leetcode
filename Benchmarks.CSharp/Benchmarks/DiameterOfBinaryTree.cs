b04abb0be8fa1697dec8402d79c373e66c8f33d0
Simon McKenna <sshnug.si+github@gmail.com>
2022-07-17 14:08:50 +0930

    Add "Diameter of Binary Tree" challenge

---
+++ Benchmarks.CSharp/Benchmarks/DiameterOfBinaryTree.cs
@@ -0,0 +1,19 @@
+﻿// ReSharper disable once CheckNamespace
+namespace LeetCode;
+
+using BenchmarkDotNet.Attributes;
+
+public partial class CSharpBenchmarks
+{
+    [Benchmark]
+    public void DiameterOfBinaryTree()
+    {
+        var root1 = new TreeNode(1,
+            left: new TreeNode(2,
+                left: new TreeNode(4), right: new TreeNode(5)),
+            right: new TreeNode(3));
+        var root2 = new TreeNode(1, left: new TreeNode(2));
+        Submission.DiameterOfBinaryTree(root1);
+        Submission.DiameterOfBinaryTree(root2);
+    }
+}
