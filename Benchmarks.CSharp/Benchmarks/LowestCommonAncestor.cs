67377c375253578da80382d18a5b7b94ac94f57d
Simon McKenna <sshnug.si+github@gmail.com>
2022-07-24 13:19:04 +0930

    Add "Lowest Common Ancestor of a Binary Search Tree" challenge

---
+++ Benchmarks.CSharp/Benchmarks/LowestCommonAncestor.cs
@@ -0,0 +1,21 @@
+﻿// ReSharper disable once CheckNamespace
+namespace LeetCode;
+
+using BenchmarkDotNet.Attributes;
+
+public partial class CSharpBenchmarks
+{
+    [Benchmark]
+    public void LowestCommonAncestor()
+    {
+        var root1 = new TreeNode(new int?[] { 6, 2, 8, 0, 4, 7, 9, null, null, 3, 5 });
+        var p1 = root1.left!;
+        var q1 = root1.right!;
+        var q2 = p1.right!;
+        var root3 = new TreeNode(new int?[] { 2, 1 });
+        var q3 = root3.left!;
+        Submission.LowestCommonAncestor(root1, p1, q1);
+        Submission.LowestCommonAncestor(root1, p1, q2);
+        Submission.LowestCommonAncestor(root3, root3, q3);
+    }
+}
