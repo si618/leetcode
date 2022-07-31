327c7c9102f2cf694b9922b3c7bb3f9ffe9fe3fe
Simon McKenna <sshnug.si+github@gmail.com>
2022-07-31 12:45:34 +0930

    Use simpler ctor for ListNode

--- Benchmarks.CSharp/Benchmarks/AddTwoNumbers.cs
+++ Benchmarks.CSharp/Benchmarks/AddTwoNumbers.cs
@@ -7,25 +7,12 @@ public partial class CSharpBenchmarks
     [Benchmark]
     public void AddTwoNumbers()
     {
-        var ex11 = new ListNode(2,
-            new ListNode(4,
-            new ListNode(3)));
-        var ex12 = new ListNode(5,
-            new ListNode(6,
-            new ListNode(4)));
+        var ex11 = new ListNode(new[] { 2, 4, 3 });
+        var ex12 = new ListNode(new[] { 5, 6, 4 });
         var ex21 = new ListNode();
         var ex22 = new ListNode();
-        var ex31 = new ListNode(9,
-            new ListNode(9,
-            new ListNode(9,
-            new ListNode(9,
-            new ListNode(9,
-            new ListNode(9,
-            new ListNode(9)))))));
-        var ex32 = new ListNode(9,
-            new ListNode(9,
-            new ListNode(9,
-            new ListNode(9))));
+        var ex31 = new ListNode(new[] { 9, 9, 9, 9, 9, 9, 9 });
+        var ex32 = new ListNode(new[] { 9, 9, 9, 9 });
         Submission.AddTwoNumbers(ex11, ex12);
         Submission.AddTwoNumbers(ex21, ex22);
         Submission.AddTwoNumbers(ex31, ex32);
