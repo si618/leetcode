bb8e99a0c79323e72a864b1792336c6548250af2
Simon McKenna <sshnug.si+github@gmail.com>
2022-07-31 15:11:55 +0930

    Add "Subsets" challenge

---
+++ Benchmarks.CSharp/Benchmarks/Subsets.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode;
+
+using BenchmarkDotNet.Attributes;
+
+public partial class CSharpBenchmarks
+{
+    [Benchmark]
+    public void Subsets()
+    {
+        Challenge.Subsets(new[] { 1, 2, 3 });
+        Challenge.Subsets(new[] { 0 });
+    }
+}
