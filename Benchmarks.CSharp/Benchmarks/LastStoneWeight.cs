7b7e429866f91aaf66e07802efbe1a8da7e2e3f6
Simon McKenna <sshnug.si+github@gmail.com>
2022-07-31 13:10:18 +0930

    Add "Last Stone Weight" challenge

---
+++ Benchmarks.CSharp/Benchmarks/LastStoneWeight.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode;
+
+using BenchmarkDotNet.Attributes;
+
+public partial class CSharpBenchmarks
+{
+    [Benchmark]
+    public void LastStoneWeight()
+    {
+        Submission.LastStoneWeight(new[] { 2,7,4,1,8,1 });
+        Submission.LastStoneWeight(new[] { 1 });
+    }
+}
---
+++ Submissions.CSharp/Challenges/LastStoneWeight.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode;
+
+using FluentAssertions;
+using FluentAssertions.Execution;
+using NUnit.Framework;
+
+public sealed partial class Submission
+{
+    [LeetCode("Last Stone Weight", Difficulty.Easy, Category.HeapPriorityQueue)]
+    // ReSharper disable once ParameterTypeCanBeEnumerable.Global
+    public static int LastStoneWeight(int[] stones)
+    {
+        PriorityQueue<int, int> queue = new();
+        foreach (var stone in stones)
+        {
+            queue.Enqueue(stone, -stone);
+        }
+
+        while (queue.Count > 1)
+        {
+            var heaviest = queue.Dequeue();
+            var nextHeaviest = queue.Dequeue();
+            var remainder = heaviest - nextHeaviest;
+            if (remainder > 0)
+            {
+                queue.Enqueue(remainder, -remainder);
+            }
+        }
+        return queue.Count == 0 ? 0 : queue.Peek();
+    }
+
+    [Test]
+    public void LastStoneWeightTest()
+    {
+        var ex1 = new[] { 2,7,4,1,8,1 };
+        var ex2 = new[] { 1 };
+
+        LastStoneWeight(ex1).Should().Be(1);
+        LastStoneWeight(ex2).Should().Be(1);
+    }
+}
