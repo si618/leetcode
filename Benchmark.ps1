239f5bfbc9b2ba00cdbe902c70b5c24eeac618bb
Simon McKenna <sshnug.si+github@gmail.com>
2022-08-13 13:23:09 +0930

    Be nicer to github by reducing benchmark counts

--- Benchmark.ps1
+++ Benchmark.ps1
@@ -1,2 +1,2 @@
-dotnet run --project Benchmarks.CSharp/Benchmarks.CSharp.csproj --configuration Release --filter LeetCode.CSharpBenchmarks* --memory --exporters json --warmupCount 2 --minIterationCount 4 --maxIterationCount 6
-dotnet run --project Benchmarks.FSharp/Benchmarks.FSharp.fsproj --configuration Release --filter LeetCode.FSharpBenchmarks* --memory --exporters json --warmupCount 2 --minIterationCount 4 --maxIterationCount 6
+dotnet run --project Benchmarks.CSharp/Benchmarks.CSharp.csproj --configuration Release --filter LeetCode.CSharpBenchmarks* --memory --exporters json --warmupCount 3 --minIterationCount 6 --maxIterationCount 9
+dotnet run --project Benchmarks.FSharp/Benchmarks.FSharp.fsproj --configuration Release --filter LeetCode.FSharpBenchmarks* --memory --exporters json --warmupCount 3 --minIterationCount 6 --maxIterationCount 9
