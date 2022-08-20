fe420a6782317b01947b1c426573279bfedbb1fb
Simon McKenna <sshnug.si+github@gmail.com>
2022-08-20 17:03:43 +0930

    Add "Climbing Stairs" problem

---
+++ Benchmarks.CSharp/Benchmarks/ClimbStairs.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode;
+
+using BenchmarkDotNet.Attributes;
+
+public partial class CSharpBenchmarks
+{
+    [Benchmark]
+    public void ClimbStairs()
+    {
+        Problem.ClimbStairs(2);
+        Problem.ClimbStairs(3);
+    }
+}
---
+++ LeetCode.CSharp/Problems/ClimbStairs.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode;
+
+using FluentAssertions;
+using NUnit.Framework;
+
+public sealed partial class Problem
+{
+    [LeetCode("Climbing Stairs", Difficulty.Easy, Category.OneDDynamicProgramming)]
+    public static int ClimbStairs(int n)
+    {
+        // End of decision tree always has one step as two steps would be out of bounds
+        var oneStep = 1;
+        var twoStep = 1;
+
+        // Start from the top of the stairs and work backwards
+        for (var i = 0; i < n - 1; i++)
+        {
+            // Memoize result to avoid recalculation
+            var previousStep = oneStep;
+            oneStep += twoStep;
+            twoStep = previousStep;
+        }
+
+        return oneStep;
+    }
+
+    [Test]
+    public void ClimbStairsTest()
+    {
+        ClimbStairs(2).Should().Be(2);
+        ClimbStairs(3).Should().Be(3);
+    }
+}
