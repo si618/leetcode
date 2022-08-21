5080fb2d465d9d9f3ba7060c4929a58928ad6311
Simon McKenna <sshnug.si+github@gmail.com>
2022-08-21 10:55:39 +0930

    Remove argument range check as leetcode rejects it 🙄

--- LeetCode.CSharp/Problems/MinCostClimbingStairs.cs
+++ LeetCode.CSharp/Problems/MinCostClimbingStairs.cs
@@ -12,15 +12,8 @@ public sealed partial class Problem
         Category.OneDDynamicProgramming)]
     public static int MinCostClimbingStairs(int[] cost)
     {
-        if (cost.Length is < 3 or > 999)
-        {
-            throw new ArgumentOutOfRangeException(nameof(cost));
-        }
-
         // Add zero to end of array with no cost to cater for last step
-        var minCost = new int[cost.Length + 1];
-        Array.Copy(cost, minCost, cost.Length);
-        minCost[^1] = 0;
+        var minCost = cost.Append(0).ToArray();
 
         // Start from the top of the stairs and work backwards to solve each sub-problem
         for (var i = minCost.Length - 3; i >= 0; i--)
@@ -35,11 +28,6 @@ public sealed partial class Problem
     [Test]
     public void MinCostClimbingStairsTest()
     {
-            Action lessThan = () => MinCostClimbingStairs(new int [2]);
-            Action moreThan = () => MinCostClimbingStairs(new int [1_000]);
-
-            lessThan.Should().Throw<ArgumentOutOfRangeException>();
-            moreThan.Should().Throw<ArgumentOutOfRangeException>();
             MinCostClimbingStairs(new []{ 10, 15, 20 }).Should().Be(15);
             MinCostClimbingStairs(new []{ 1, 100, 1, 1, 1, 100, 1, 1, 100, 1}).Should().Be(6);
     }
