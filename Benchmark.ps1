e2ba400f6a5e2961bb3a6347c00aa5521a416103
Simon McKenna <sshnug.si+github@gmail.com>
2022-08-22 16:00:00 +0930

    Use benchmark defaults

--- Benchmark.ps1
+++ Benchmark.ps1
@@ -1,2 +1,2 @@
-dotnet run --project Benchmarks.CSharp/Benchmarks.CSharp.csproj --configuration Release --filter LeetCode.CSharpBenchmarks* --memory --exporters json --warmupCount 3 --minIterationCount 6 --maxIterationCount 9
-dotnet run --project Benchmarks.FSharp/Benchmarks.FSharp.fsproj --configuration Release --filter LeetCode.FSharpBenchmarks* --memory --exporters json --warmupCount 3 --minIterationCount 6 --maxIterationCount 9
+dotnet run --project Benchmarks.CSharp/Benchmarks.CSharp.csproj --configuration Release --filter LeetCode.CSharpBenchmarks* --memory
+dotnet run --project Benchmarks.FSharp/Benchmarks.FSharp.fsproj --configuration Release --filter LeetCode.FSharpBenchmarks* --memory
