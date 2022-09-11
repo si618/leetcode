7222858bb17d3201da60090d16a214040b5833b1
Simon McKenna <sshnug.si+github@gmail.com>
2022-09-11 12:24:20 +0930

    Add "Maximum Subarray" problem

---
+++ Benchmarks.CSharp/Benchmarks/MaxSubarray.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode;
+
+public partial class CSharpBenchmarks
+{
+    [Benchmark]
+    public void MaxSubarray()
+    {
+        // Inputs from LeetCode
+        var ex1 = new[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        var ex2 = new[] { 1 };
+        var ex3 = new[] { 5, 4, -1, 7, 8 };
+
+        // Larger inputs for benchmark
+        var random = new Random(618);
+        var ex4 = new int[100_000];
+        for (var i = 0; i < ex4.Length; i++)
+        {
+            // Generate some negative and positive integers
+            ex4[i] = random.Next(1, 20) - 10;
+        }
+
+        Problem.MaxSubarray(ex1);
+        Problem.MaxSubarray(ex2);
+        Problem.MaxSubarray(ex3);
+        Problem.MaxSubarray(ex4);
+    }
+}
---
+++ LeetCode.CSharp/Problems/MaxSubArrary.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode;
+
+public sealed partial class Problem
+{
+    [LeetCode("Maximum Subarray", Difficulty.Medium, Category.Greedy, "5WZl3MMT0Eg")]
+    public static int MaxSubarray(int[] nums)
+    {
+        var maxSub = nums[0];
+        var curSum = 0;
+
+        foreach (var i in nums)
+        {
+            // Reset if previous calculation was negative
+            if (curSum < 0)
+            {
+                curSum = 0;
+            }
+            curSum += i;
+            maxSub = Math.Max(curSum, maxSub);
+        }
+
+        return maxSub;
+    }
+
+    [Test]
+    public void MaxSubarrayTest()
+    {
+        var ex1 = new[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        var ex2 = new[] { 1 };
+        var ex3 = new[] { 5, 4, -1, 7, 8 };
+
+        MaxSubarray(ex1).Should().Be(6);
+        MaxSubarray(ex2).Should().Be(1);
+        MaxSubarray(ex3).Should().Be(23);
+    }
+}
