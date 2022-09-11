ef8195fe925558e4e1b3ba9ea54096d2ffe93abc
Simon McKenna <sshnug.si+github@gmail.com>
2022-09-11 12:24:45 +0930

    Comment for clarity

--- Benchmarks.CSharp/Benchmarks/UniquePaths.cs
+++ Benchmarks.CSharp/Benchmarks/UniquePaths.cs
@@ -5,8 +5,11 @@ public partial class CSharpBenchmarks
     [Benchmark]
     public void UniquePaths()
     {
+        // Inputs from LeetCode
         Problem.UniquePaths(7, 3);
         Problem.UniquePaths(3, 2);
+
+        // Larger inputs for benchmark
         Problem.UniquePaths(10_000, 100);
     }
 }
