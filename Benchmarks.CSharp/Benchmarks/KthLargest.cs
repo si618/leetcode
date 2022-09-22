efe95c89977fbdbfc7e907e54358e94d58cab6d1
Simon McKenna <sshnug.si+github@gmail.com>
2022-09-22 13:12:51 +0930

    Lower array size as borking on github

--- Benchmarks.CSharp/Benchmarks/KthLargest.cs
+++ Benchmarks.CSharp/Benchmarks/KthLargest.cs
@@ -7,13 +7,13 @@ public partial class CSharpBenchmarks
     [GlobalSetup(Target = nameof(KthLargest))]
     public void KthLargestSetup()
     {
-        var values = Enumerable.Range(1, 1_000_000_000).ToArray();
-        _kthLargest = new Problem.KthLargest(500_000_000, values);
+        var values = Enumerable.Range(1, 100_000_000).ToArray();
+        _kthLargest = new Problem.KthLargest(50_000_000, values);
     }
 
     [Benchmark]
     public int KthLargest()
     {
-        return _kthLargest.Add(750_000_000);
+        return _kthLargest.Add(75_000_000);
     }
 }
