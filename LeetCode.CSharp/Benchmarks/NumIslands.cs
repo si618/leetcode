5b04b692202bd7ee636fecf749872a4051034d42
Simon McKenna <sshnug.si+github@gmail.com>
2022-10-15 14:34:54 +1030

    Fix NumIslands benchmark

--- LeetCode.CSharp/Benchmarks/NumIslands.cs
+++ LeetCode.CSharp/Benchmarks/NumIslands.cs
@@ -2,31 +2,34 @@
 
 public partial class Benchmark
 {
-    // private char[][] CharArrayMulti { get; set; } = { new[] { char.MinValue }};
+    private char[][] CharArrayMulti { get; set; } = { new[] { char.MinValue }};
 
     [GlobalSetup(Target = nameof(NumIslands))]
     public void NumIslandsSetup()
     {
-        // CharArrayMulti = new char[10_000][];
-        // for (var i = 0; i < CharArrayMulti.Length; i++)
-        // {
-        //     for (var j = 0; j < CharArrayMulti.Length; j++)
-        //     {
-        //         CharArrayMulti[i][j] = i % 10 == 0 ? '1' : '0';
-        //     }
-        // }
+        var random = new Random(618);
+
+        CharArrayMulti = new char[1_000][];
+        for (var i = 0; i < CharArrayMulti.Length; i++)
+        {
+            CharArrayMulti[i] = new char[1_000];
+            for (var j = 0; j < CharArrayMulti[i].Length; j++)
+            {
+                var isLand = j % random.Next(1, 10) == 0;
+                CharArrayMulti[i][j] = isLand ? '1' : '0';
+            }
+        }
     }
 
     [Benchmark]
     public int NumIslands()
     {
-        return 0;
-        // return Problem.NumIslands(CharArrayMulti);
+        return Problem.NumIslands(CharArrayMulti);
     }
 
     [GlobalCleanup(Target = nameof(NumIslands))]
     public void NumIslandsCleanup()
     {
-        // CharArrayMulti = new[] { new[] { char.MinValue } };
+        CharArrayMulti = new[] { new[] { char.MinValue } };
     }
 }
