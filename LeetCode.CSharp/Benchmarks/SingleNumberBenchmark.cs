3ce4c19c06a7ecc76daf91cceaab0e3005082c38
Simon McKenna <sshnug.si+github@gmail.com>
2022-11-20 15:37:51 +1030

    Add "Single Number" problem

---
+++ LeetCode.CSharp/Benchmarks/SingleNumberBenchmark.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.CSharp.Benchmarks;
+
+public class SingleNumberBenchmark : Benchmark
+{
+    [GlobalSetup(Target = nameof(SingleNumber))]
+    public void SingleNumberSetup()
+    {
+        IntArray1 = Enumerable.Range(1, 1_000_000)
+            .Concat(Enumerable.Range(1, 1_000_001))
+            .ToArray();
+    }
+
+    [Benchmark]
+    public int SingleNumber() => Problem.SingleNumber(IntArray1);
+
+    [GlobalCleanup(Target = nameof(SingleNumber))]
+    public void SingleNumberCleanup()
+    {
+        IntArray1 = Array.Empty<int>();
+    }
+}
---
+++ LeetCode.CSharp/Problems/SingleNumber.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.CSharp.Problems;
+
+public sealed partial class Problem
+{
+    [LeetCode(
+        "Single Number",
+        Difficulty.Easy,
+        Category.BitManipulation,
+        "https://www.youtube.com/watch?v=qMPX1AOa83k")]
+    [SuppressMessage("ReSharper", "ParameterTypeCanBeEnumerable.Global")]
+    public static int SingleNumber(int[] digits) => digits.Aggregate((x, y) => x ^ y);
+
+    [Fact]
+    public void SingleNumberTest()
+    {
+        var ex1 = new[] { 2, 2, 1 };
+        var ex2 = new[] { 4, 1, 2, 1, 2 };
+        var ex3 = new[] { 1 };
+
+        SingleNumber(ex1).Should().Be(1);
+        SingleNumber(ex2).Should().Be(4);
+        SingleNumber(ex3).Should().Be(1);
+    }
+}
