85b5a282f7654d4a172a7ee3801b5e8d4d4394fd
Simon McKenna <sshnug.si+github@gmail.com>
2022-11-20 15:14:50 +1030

    Add "Plus One" problem

---
+++ LeetCode.CSharp/Benchmarks/PlusOneBenchmark.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.CSharp.Benchmarks;
+
+public class PlusOneBenchmark : Benchmark
+{
+    [GlobalSetup(Target = nameof(PlusOne))]
+    public void PlusOneSetup()
+    {
+        IntArray1 = Enumerable.Range(1, 1_000_000)
+            .Concat(Enumerable.Range(1, 1_000_000))
+            .ToArray();
+    }
+
+    [Benchmark]
+    public int[] PlusOne() => Problem.PlusOne(IntArray1);
+
+    [GlobalCleanup(Target = nameof(PlusOne))]
+    public void PlusOneCleanup()
+    {
+        IntArray1 = Array.Empty<int>();
+    }
+}
---
+++ LeetCode.CSharp/Problems/PlusOne.cs
@@ -0,0 +1,57 @@
+﻿namespace LeetCode.CSharp.Problems;
+
+public sealed partial class Problem
+{
+    [LeetCode(
+        "Plus One",
+        Difficulty.Easy,
+        Category.MathAndGeometry,
+        "https://www.youtube.com/watch?v=jIaA8boiG1s")]
+    [SuppressMessage("ReSharper", "ParameterTypeCanBeEnumerable.Global")]
+    public static int[] PlusOne(int[] digits)
+    {
+        var list = digits.Reverse().ToList();
+        var carry = 1;
+        var i = 0;
+
+        while (carry == 1)
+        {
+            if (i < list.Count)
+            {
+                if (list[i] == 9)
+                {
+                    list[i] = 0;
+                    carry = 1;
+                }
+                else
+                {
+                    list[i] += 1;
+                    carry = 0;
+                }
+            }
+            else
+            {
+                list.Add(1);
+                carry = 0;
+            }
+
+            i++;
+        }
+
+        list.Reverse();
+
+        return list.ToArray();
+    }
+
+    [Fact]
+    public void PlusOneTest()
+    {
+        var ex1 = new[] { 1, 2, 3 };
+        var ex2 = new[] { 4, 3, 2, 1 };
+        var ex3 = new[] { 9 };
+
+        PlusOne(ex1).Should().Equal(1, 2, 4);
+        PlusOne(ex2).Should().Equal(4, 3, 2, 2);
+        PlusOne(ex3).Should().Equal(1, 0);
+    }
+}
