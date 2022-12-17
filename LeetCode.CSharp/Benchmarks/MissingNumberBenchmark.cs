7e17046ba0c1bbf9056dc11f2ce53e5f7cd38df7
Simon McKenna <sshnug.si+github@gmail.com>
2022-12-17 14:30:28 +1030

    Add "Missing Number" problem

---
+++ LeetCode.CSharp/Benchmarks/MissingNumberBenchmark.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.CSharp.Benchmarks;
+
+public class MissingNumberBenchmark : Benchmark
+{
+    [GlobalSetup(Target = nameof(MissingNumber))]
+    public void MissingNumberSetup() =>
+        IntArray1 = Enumerable.Range(0, 1_000_000).Except(new[] { 100_000 }).ToArray();
+
+    [Benchmark]
+    public int MissingNumber() => Problem.MissingNumber(IntArray1);
+
+    [GlobalCleanup(Target = nameof(MissingNumber))]
+    public void MissingNumberCleanup() => IntArray1 = Array.Empty<int>();
+}
---
+++ LeetCode.CSharp/Problems/MissingNumber.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.CSharp.Problems;
+
+public sealed partial class Problem
+{
+    [LeetCode(
+    "Missing Number",
+    Difficulty.Easy,
+    Category.BitManipulation,
+    "https://www.youtube.com/watch?v=WnPLSRLSANE")]
+    [SuppressMessage("ReSharper", "ParameterTypeCanBeEnumerable.Global")]
+    public static int MissingNumber(int[] nums) =>
+        nums.Length + Enumerable.Range(0, nums.Length).Sum(i => i - nums[i]);
+
+    [Fact]
+    public void MissingNumberTest()
+    {
+        var ex1 = new[] { 3, 0, 1 };
+        var ex2 = new[] { 0, 1 };
+        var ex3 = new[] { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
+
+        MissingNumber(ex1).Should().Be(2);
+        MissingNumber(ex2).Should().Be(2);
+        MissingNumber(ex3).Should().Be(8);
+    }
+}
