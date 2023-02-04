8f786f77e257abb1b9253ae36f33637f16442857
Simon McKenna <sshnug.si+github@gmail.com>
2023-02-04 14:29:33 +1030

    Add "Encode and Decode Strings" problem

---
+++ LeetCode.CSharp/Benchmarks/EncodeDecodeBenchmark.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode.CSharp.Benchmarks;
+
+public class EncodeDecodeBenchmark : Benchmark
+{
+    [GlobalSetup(Target = nameof(Encode))]
+    public void EncodeSetup()
+    {
+        StringArray1 = new string[1_000];
+
+        for (var i = 0; i < StringArray1.Length; i++)
+        {
+            StringArray1[i] = BuildPseudoRandomString(Random.Next(1_000), 2);
+        }
+    }
+
+    [GlobalSetup(Target = nameof(Decode))]
+    public void DecodeSetup()
+    {
+        EncodeSetup();
+
+        String1 = Problem.Encode(StringArray1);
+    }
+
+    [Benchmark]
+    public string Encode() => Problem.Encode(StringArray1);
+
+    [Benchmark]
+    public IEnumerable<string> Decode() => Problem.Decode(String1);
+
+    [GlobalCleanup(Target = nameof(Encode))]
+    public void EncodeCleanup() => StringArray1 = Array.Empty<string>();
+
+    [GlobalCleanup(Target = nameof(Decode))]
+    public void DecodeCleanup() => String1 = string.Empty;
+}
---
+++ LeetCode.CSharp/Problems/EncodeDecode.cs
@@ -0,0 +1,70 @@
+﻿namespace LeetCode.CSharp.Problems;
+
+public sealed partial class Problem
+{
+    private const char EncodeDecodeDelimiter = ':';
+
+    [LeetCode("Encode and Decode Strings (Encode)",
+        Difficulty.Medium,
+        Category.ArraysAndHashing,
+        "https://www.youtube.com/watch?v=B1k_sxOSgv8")]
+    public static string Encode(IEnumerable<string> strs) =>
+        string.Concat(strs.SelectMany(s => $"{s.Length}{EncodeDecodeDelimiter}{s}"));
+
+    [LeetCode("Encode and Decode Strings (Decode)",
+        Difficulty.Medium,
+        Category.ArraysAndHashing,
+        "https://www.youtube.com/watch?v=B1k_sxOSgv8")]
+    public static IEnumerable<string> Decode(string str)
+    {
+        var result = new List<string>();
+        var i = 0;
+
+        while (i < str.Length)
+        {
+            var j = i;
+
+            // Find length of encoded string
+            while (str[j] != EncodeDecodeDelimiter)
+            {
+                j++;
+            }
+
+            // Assumes string is correctly encoded
+            var length = int.Parse(str.AsSpan(i, j - i));
+
+            // Move past delimiter
+            j++;
+
+            // Extract substring
+            result.Add(str.AsSpan(j, length).ToString());
+
+            // Move to next encoded string or end
+            i = j + length;
+        }
+
+        return result;
+    }
+
+    [Fact]
+    public void EncodeTest()
+    {
+        var ex1 = new List<string> { "abc" };
+        var ex2 = new List<string> { "abc", "c : a" };
+
+        Encode(Array.Empty<string>()).Should().Be(string.Empty);
+        Encode(ex1).Should().Be("3:abc");
+        Encode(ex2).Should().Be("3:abc5:c : a");
+    }
+
+    [Fact]
+    public void DecodeTest()
+    {
+        const string ex1 = "3:abc";
+        const string ex2 = "3:abc5:c : a";
+
+        Decode(string.Empty).Should().BeEmpty();
+        Decode(ex1).Should().Equal("abc");
+        Decode(ex2).Should().Equal("abc", "c : a");
+    }
+}
